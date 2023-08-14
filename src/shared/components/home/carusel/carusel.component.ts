d6b6f288f693220973dbe16aa820481fc58073ca
= <jasurbekyusufov@gmail.com>
2023-08-14 15:08:16 +0500

    PAGES: Home Page -> WIP

--- src/shared/components/home/carusel/carusel.component.ts
+++ src/shared/components/home/carusel/carusel.component.ts
@@ -10,9 +10,9 @@ export class CaruselComponent {
   @ViewChild(NzCarouselComponent) carousel!: NzCarouselComponent;
 
   array = [
-    '../../../assets/images/download (2).jpg',
-    '../../../assets/images/download.jpg',
-    '../../../assets/images/download (3).jpg',
+    '../../../../assets/images/carusel/Air-New-Zealand-Boeing-747-400.webp',
+    '../../../../assets/images/carusel/airplane-flying-in-sky-jet-plane-fly-in-clouds-airplanes-travel-and-vacation-aircraft-flight-plane-airplane-trip-to-airport-or-airline-transportation-flat-airplane-illustration-free-vector.jpg',
+    '../../../../assets/images/carusel/airplane-windows.jpg',
   ];
 
   prevSlide() {
