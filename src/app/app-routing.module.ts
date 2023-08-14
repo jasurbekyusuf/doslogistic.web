e8f7b59fcb9bfdb0a7b33271c18a8d95d640559c
= <jasurbekyusufov@gmail.com>
2023-08-14 17:33:25 +0500

    PAGES: Add About Us page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './modules/auth/login/login.component';
 import { RegisterComponent } from './modules/auth/register/register.component';
 import { HomeComponent } from './modules/layouts/home/home.component';
+import { AboutUsComponent } from './modules/layouts/about-us/about-us.component';
 
 
 const routes: Routes = [
   // { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
+  { path: 'aboutUs', component: AboutUsComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
 ];
