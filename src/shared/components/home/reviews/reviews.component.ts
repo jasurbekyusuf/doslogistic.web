a6e4ae4dbdbad28f4c4bf7efbb4187c6b49bfb32
bilolkobilov <bilol.kobilov@s.amity.edu>
2023-08-20 17:50:15 +0500

    CODE RUB: Add Carausel Review

--- src/shared/components/home/reviews/reviews.component.ts
+++ src/shared/components/home/reviews/reviews.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-reviews',
@@ -6,5 +6,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./reviews.component.scss']
 })
 export class ReviewsComponent {
+  @ViewChild('container') containerRef!: ElementRef;
+  obj: number = 0;
+  autoplayInterval: any;
+  autoplaySpeed: number = 4000;
 
+  ngAfterViewInit() {
+    this.obj = 1365;
+    this.startAutoplay();
+  }
+
+  ngOnDestroy() {
+    this.stopAutoplay();
+  }
+
+  startAutoplay() {
+    this.autoplayInterval = setInterval(() => {
+      this.goNext();
+    }, this.autoplaySpeed);
+  }
+
+  stopAutoplay() {
+    if (this.autoplayInterval) {
+      clearInterval(this.autoplayInterval);
+    }
+  }
+
+  goNext() {
+    this.containerRef.nativeElement.scrollLeft += this.obj;
+    if (this.containerRef.nativeElement.scrollLeft >= this.containerRef.nativeElement.scrollWidth - this.obj) {
+      this.containerRef.nativeElement.scrollLeft = 0;
+    }
+  }
+
+  goPrevious() {
+    this.containerRef.nativeElement.scrollBy({
+      left: -this.obj
+    });
+    if (this.containerRef.nativeElement.scrollLeft <= 0) {
+      this.containerRef.nativeElement.scrollLeft = this.containerRef.nativeElement.scrollWidth - this.obj;
+    }
+  }
+
+  pauseAutoplay() {
+    this.stopAutoplay();
+  }
 }
