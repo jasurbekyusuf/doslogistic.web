b8fed19ccebdf9ed628e6a3fd37b8f275fa53c67
bilolkobilov <bilol.kobilov@s.amity.edu>
2023-08-22 01:56:51 +0500

    NAVBAR: Add Media

--- src/app/modules/navbar/navbar.component.ts
+++ src/app/modules/navbar/navbar.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { DropDownAnimation } from './animation';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  animations: [DropDownAnimation]
 })
 export class NavbarComponent {
-  Navbar= NavbarComponent;
+  Navbar = NavbarComponent;
+  isActive = false;
+  isOpen1 = false;
+  isOpen: boolean = false;
+
+  menuBtnFunction() {
+    this.isOpen1 = !this.isOpen1;
+    this.isActive = this.isOpen1;
+  }
 }
