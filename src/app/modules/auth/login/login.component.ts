008234988f81fcb90e7c6b2d01ceec412fe07ef3
bilolkobilov <bilol.kobilov@s.amity.edu>
2023-08-29 20:34:02 +0500

    PAGES: Add login

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+declare const intlTelInput: any;
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  showPassword: boolean = false;
+  showRepeatPassword: boolean = false;
+  checked = false;
 
+  ngOnInit() {
+    const mobileCodeInput = document.getElementById('mobile_code') as HTMLInputElement;
+    if (mobileCodeInput) {
+      const iti = intlTelInput(mobileCodeInput, {
+        initialCountry: 'uz',
+        separateDialCode: true,
+      });
+      mobileCodeInput.addEventListener('input', () => {
+        mobileCodeInput.setCustomValidity('');
+      });
+    }
+  }
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+
+  toggleRepeatPasswordVisibility() {
+    this.showRepeatPassword = !this.showRepeatPassword;
+  }
 }
