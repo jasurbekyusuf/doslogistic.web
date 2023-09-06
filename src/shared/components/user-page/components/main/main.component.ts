d7295a9794cb6e7e62337487d28b783bb6508d53
bilolkobilov <bilol.kobilov@s.amity.edu>
2023-09-06 20:04:41 +0500

    WIP

--- src/shared/components/user-page/components/main/main.component.ts
+++ src/shared/components/user-page/components/main/main.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent {
+  modalOpen = false;
+  currentStep = 0;
+  totalSteps = 5;
+  selectedCountry: string | null = null;
 
+  selectCountry(country: string) {
+    this.selectedCountry = country;
+    this.currentStep = 1;
+  }
+
+  openModal() {
+    this.modalOpen = true;
+    this.currentStep = 0;
+  }
+
+  closeModal() {
+    this.modalOpen = false;
+  }
+
+  nextStep() {
+    if (this.currentStep < this.totalSteps) {
+      this.currentStep++;
+    }
+  }
+
+  prevStep() {
+    if (this.currentStep > 1) {
+      this.currentStep--;
+    }
+  }
+
+  get orderSteps(): number[] {
+    return Array(this.totalSteps).fill(0).map((_, index) => index + 1);
+  }
 }
