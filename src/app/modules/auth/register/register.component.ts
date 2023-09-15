00d9ab8776ed96088b99992ecd22801a09ab2bb8
bilolkobilov <bilol.kobilov@s.amity.edu>
2023-09-15 20:41:22 +0500

    Add validation

--- src/app/modules/auth/register/register.component.ts
+++ src/app/modules/auth/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import Validation from './validation';
 declare const intlTelInput: any;
 
 @Component({
@@ -9,7 +11,17 @@ declare const intlTelInput: any;
 export class RegisterComponent {
   showPassword: boolean = false;
   showRepeatPassword: boolean = false;
-  checked = false;
+  submitted = false;
+
+  form: FormGroup = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+    phoneNumber: new FormControl(''),
+    acceptTerms: new FormControl(false),
+  });
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     const mobileCodeInput = document.getElementById('mobile_code') as HTMLInputElement;
@@ -22,7 +34,35 @@ export class RegisterComponent {
         mobileCodeInput.setCustomValidity('');
       });
     }
+
+    this.form = this.formBuilder.group(
+      {
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(40)]],
+        confirmPassword: ['', Validators.required],
+        acceptTerms: [false, [Validators.required, Validators.requiredTrue]],
+        phoneNumber: ['', [Validators.required, Validators.minLength(9)]]
+      },
+      {
+        validators: [Validation.match('password', 'confirmPassword')],
+      }
+    );
   }
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.form.invalid) {
+      return;
+    }
+
+    console.log(JSON.stringify(this.form.value, null, 2));
+  }
+
   togglePasswordVisibility() {
     this.showPassword = !this.showPassword;
   }
