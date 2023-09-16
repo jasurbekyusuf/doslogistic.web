c2015fbd0cd128acbef00df1f132c75682a8ddf0
bilolkobilov <bilol.kobilov@s.amity.edu>
2023-09-16 23:31:00 +0500

    Add dashboard

--- src/shared/components/user-page/components2/my-parcels/my-parcels.component.ts
+++ src/shared/components/user-page/components2/my-parcels/my-parcels.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as echarts from 'echarts';
 
 @Component({
   selector: 'app-my-parcels',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./my-parcels.component.scss']
 })
 export class MyParcelsComponent {
+  ngOnInit(): void {
+    const chartDom = document.getElementById('main')!;
+    const myChart = echarts.init(chartDom);
+    let option: echarts.EChartsOption;
 
+    option = {
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      },
+      yAxis: {
+        type: 'value',
+      },
+      series: [
+        {
+          data: [820, 932, 901, 934, 1290, 1330, 1320],
+          type: 'line',
+          areaStyle: { color: 'rgb(5, 140, 66)' },
+        },
+      ],
+    };
+
+    option && myChart.setOption(option);
+  }
 }
