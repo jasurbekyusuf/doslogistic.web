1c7eb63fbbd1a0ceceb3c40465617962eb1277d4
bilolkobilov <bilol.kobilov@s.amity.edu>
2023-09-21 23:21:36 +0500

    Add calculator

--- src/app/modules/navbar/navbar.component.ts
+++ src/app/modules/navbar/navbar.component.ts
@@ -29,6 +29,11 @@ export class NavbarComponent {
   isHomePage: boolean = false;
   isActive = false;
   isOpen = false;
+  modalOpen = false;
+
+  weightValue: number = 0;
+  selectedUnit: string = 'kg';
+  sliderValue: number = 0;
 
   @HostBinding('@slideInOut') get slideInOut() {
     return this.isOpen ? 'open' : 'closed';
@@ -59,5 +64,38 @@ export class NavbarComponent {
         this.closeMenu();
       }
     });
+  }
+
+  openModal() {
+    this.modalOpen = true;
+    this.isOpen = !this.isOpen;
+  }
+
+  closeModal() {
+    this.modalOpen = false;
+  }
+
+  onWeightInputChange(): void {
+    this.sliderValue = parseFloat(this.weightValue.toString());
+  }
+
+  onSliderChange(event: any): void {
+    this.sliderValue = parseFloat(event.target.value);
+    this.weightValue = this.sliderValue;
+  }
+
+  getFormattedDeliveryCost(): string {
+    const deliveryCost = this.getDeliveryCost();
+    return deliveryCost.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+  }
+
+  getDeliveryCost(): number {
+    if (this.selectedUnit === 'kg') {
+      return this.weightValue * 12;
+    } else if (this.selectedUnit === 'funt') {
+      return this.weightValue * 6;
+    } else {
+      return 0;
+    }
   }
 }
