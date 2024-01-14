c16b3bc2efe07a56c2fb2bdb4c7af3ed25ec4007
= <jasurbekyusufov@gmail.com>
2024-01-14 15:11:45 +0500

    WIP

---
+++ src/app/account/auth/guards/auth.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgToastService } from 'ng-angular-popup';
+import {AuthService} from "../../../local-services/auth/auth.service";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard  {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private toast: NgToastService
+  ) {}
+
+  canActivate() {
+    if (this.authService.isLoggedIn()) {
+      return true;
+    } else {
+      // this.toast.error({ detail: 'Error', summary: 'Please Login First' });
+      this.router.navigate(['signin']);
+      return false;
+    }
+  }
+}
